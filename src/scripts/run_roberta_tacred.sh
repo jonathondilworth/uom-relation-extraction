5fa10e1930b43471940e5ec8da006b053c90b60a
Jonathon Dilworth <jonathon.dilworth@postgrad.manchster.ac.uk>
2025-02-23 00:04:32 +0000

    prepared modified scripts for experiments on BERT, RoBERTa, SpanBERT

--- src/scripts/run_roberta_tacred.sh
+++ src/scripts/run_roberta_tacred.sh
@@ -1,3 +1,3 @@
 for SEED in 78 23 61;
-do python train_tacred.py --model_name_or_path roberta-large --input_format typed_entity_marker_punct --seed $SEED --run_name roberta;
+do python train_tacred.py --model_name_or_path roberta-large --input_format typed_entity_marker_pos_punct --seed $SEED --run_name roberta;
 done;
